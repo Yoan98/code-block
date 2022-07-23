ceee99107a860e85044baf3b11ff7533f23d8829
huangyuan <xiaoyuan98@126.com>
2022-07-23 13:05:18 +0800

    修改websocket类

--- code/js/webSocketClient.js
+++ code/js/webSocketClient.js
@@ -1,100 +1,98 @@
-
-import { Message } from "element-ui"
 class WebSocketClient {
-  constructor (config = {}, callBack = () => {}) {
+  constructor(config = {}, callBack = () => {}, errorHandler = () => {}) {
     const defaultConfig = {
       url: "ws://192.168.0.207:9876/ws",
       timeInterval: 5 * 1000, // 超时则重连的时间
-      limitCheckCount: 5 // 限制心跳监测的次数（超过则主动断开）
-    }
-    const finalConfig = { ...defaultConfig, ...config }
+      limitCheckCount: 5, // 限制重连的次数（超过则主动断开）
+    };
+    const finalConfig = { ...defaultConfig, ...config };
 
-    this.ws = {}
-    this.url = finalConfig.url
-    this.handle = null
-    this.timeInterval = finalConfig.timeInterval
-    this.callBack = callBack
-    this.checkCount = 0 // 心跳检测次数
+    this.ws = {};
+    this.url = finalConfig.url;
+    this.handle = null;
+    this.timeInterval = finalConfig.timeInterval;
+    this.callBack = callBack;
+    this.errorHandler = errorHandler;
+    this.checkCount = 0; // 心跳检测次数
   }
 
-  init () {
-    this.ws = new WebSocket(this.url)
-    this.ws.onopen = () => this._onOpen()
-    this.ws.onmessage = (msg) => this._onMessage(msg)
-    this.ws.onclose = () => this._onClose()
-    this.ws.onerror = () => this._onError()
+  init() {
+    this.ws = new WebSocket(this.url);
+    this.ws.onopen = () => this._onOpen();
+    this.ws.onmessage = (msg) => this._onMessage(msg);
+    this.ws.onclose = () => this._onClose();
+    this.ws.onerror = () => this._onError();
   }
 
   // 发送消息
-  send (msg) {
-    this.ws.send(msg)
+  send(msg) {
+    this.ws.send(msg);
   }
 
   // 与服务端连接建立时触发
-  _onOpen () {
-    console.log("websocket连接成功")
+  _onOpen() {
+    console.log("websocket连接成功");
   }
 
   // 客户端接收到消息
-  _onMessage (msg) {
-
+  _onMessage(msg) {
     try {
       /**
        * 心跳检测的send逻辑（todo）
        * 判断消息类型是否为心跳检测类型，对应做处理
        */
-      const obj = JSON.parse(msg.data)
-      this.callBack(obj)
+      const obj = JSON.parse(msg.data);
+      this.callBack(obj);
     } catch (error) {
-      this.callBack(msg.data)
+      this.callBack(msg.data);
     }
 
     // 心跳检测
-    // this.checkServer()
+    this.checkServer();
   }
 
   // 当客户端主动断开
-  _onClose () {
-    console.log("close:" + this.ws.readyState)
-    console.log("已关闭websocket")
-    Message({
-      message: '已关闭websocket',
-      type: 'error'
-    })
-    this.ws.close()
+  _onClose() {
+    console.log("close:" + this.ws.readyState);
+    console.log("已关闭websocket");
+    this.errorHandler({
+      message: "已关闭websocket",
+      type: "close",
+    });
+    this.ws.close();
   }
 
   // 当连接发生错误
-  _onError () {
-    Message({
-      message: 'websocket连接失败,1秒后重连',
-      type: 'error'
-    })
+  _onError() {
+    this.errorHandler({
+      message: "连接错误",
+      type: "error",
+    });
     // 连接失败后，断线重练
     setTimeout(() => {
-      console.log("websocket重新连接")
-      this.init()
-    }, 1000)
+      console.log("websocket重新连接");
+      this.init();
+    }, 1000);
   }
 
   // 心跳检测
-  checkServer () {
-    this.checkCount = 0
-    clearInterval(this.handle)
+  checkServer() {
+    this.checkCount = 0;
+    clearInterval(this.handle);
     // 当每一次测试心跳检测数据的时间超过了所设时间，则断开重连
     this.handle = setInterval(() => {
       // 如果超过重连次数限制则主动断开连接
       if (this.checkCount >= this.limitCheckCount) {
-        clearInterval(this.handle)
-        this._onClose()
-        console.log('服务器已断开连接')
+        clearInterval(this.handle);
+        this._onClose();
+        console.log("服务器已断开连接");
       } else {
-        this.checkCount++
-        this._onClose()
-        this._onError()
+        this.checkCount++;
+        this._onClose();
+        this._onError();
       }
-    }, this.timeInterval + 1000)
+    }, this.timeInterval + 1000);
   }
 }
 
-export default WebSocketClient
+export default WebSocketClient;
