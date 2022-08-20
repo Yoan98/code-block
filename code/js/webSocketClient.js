4328604f8c5d3bbb7715500a6fc3581d6abbde80
huangyuan <xiaoyuan98@126.com>
2022-08-20 18:01:26 +0800

    '修改websocket逻辑'

--- code/js/webSocketClient.js
+++ code/js/webSocketClient.js
@@ -1,98 +1,84 @@
-class WebSocketClient {
-  constructor(config = {}, callBack = () => {}, errorHandler = () => {}) {
+class webSocketClient {
+  constructor (config = {}) {
     const defaultConfig = {
-      url: "ws://192.168.0.207:9876/ws",
-      timeInterval: 5 * 1000, // 超时则重连的时间
-      limitCheckCount: 5, // 限制重连的次数（超过则主动断开）
-    };
-    const finalConfig = { ...defaultConfig, ...config };
-
-    this.ws = {};
-    this.url = finalConfig.url;
-    this.handle = null;
-    this.timeInterval = finalConfig.timeInterval;
-    this.callBack = callBack;
-    this.errorHandler = errorHandler;
-    this.checkCount = 0; // 心跳检测次数
-  }
-
-  init() {
-    this.ws = new WebSocket(this.url);
-    this.ws.onopen = () => this._onOpen();
-    this.ws.onmessage = (msg) => this._onMessage(msg);
-    this.ws.onclose = () => this._onClose();
-    this.ws.onerror = () => this._onError();
-  }
-
-  // 发送消息
-  send(msg) {
-    this.ws.send(msg);
-  }
-
-  // 与服务端连接建立时触发
-  _onOpen() {
-    console.log("websocket连接成功");
-  }
-
-  // 客户端接收到消息
-  _onMessage(msg) {
-    try {
-      /**
-       * 心跳检测的send逻辑（todo）
-       * 判断消息类型是否为心跳检测类型，对应做处理
-       */
-      const obj = JSON.parse(msg.data);
-      this.callBack(obj);
-    } catch (error) {
-      this.callBack(msg.data);
+      url: 'localhost',
+      port: '80',
+      protocol: 'ws',
+      timeInterval: 6 * 1000,
+      token:''
     }
+    const finalConfig = { ...defaultConfig, ...config }
 
-    // 心跳检测
-    this.checkServer();
+    this.ws = {}
+    this.url = finalConfig.url
+    this.port = finalConfig.port
+    this.protocol = finalConfig.protocol
+    this.handle = null
+    this.timeInterval = finalConfig.timeInterval
   }
 
+  init () {
+    this.ws = new WebSocket(`${this.protocol}://${this.url}:${this.port}`)
+    this.ws.onopen = () => this.onOpen()
+    this.ws.onmessage = (msg) => this.onMessage(msg)
+    this.ws.onclose = () => this.onClose()
+    this.ws.onerror = () => this.onError()
+  }
+
+  send (msg) {
+    this.ws.send(msg)
+  }
+
+  // 与服务端连接成功
+  onOpen () {
+    this.ws.send(JSON.stringify({
+      event: 'auth',
+      message: 'Bearer ' + this.token
+    }))
+  }
+  // 客户端接收到消息
+  onMessage (msg) {
+    const obj = JSON.parse(msg.data)
+    switch (obj.event) {
+      case 'noAuth':
+        // 鉴权失败
+        console.log('token未通过')
+        break
+      case 'heartbeat':
+        // 心跳检查
+        this.checkServer()
+        this.send(JSON.stringify({
+          event: 'heartbeat',
+          message: 'pong'
+        }))
+        break
+    }
+  }
   // 当客户端主动断开
-  _onClose() {
-    console.log("close:" + this.ws.readyState);
-    console.log("已关闭websocket");
-    this.errorHandler({
-      message: "已关闭websocket",
-      type: "close",
-    });
-    this.ws.close();
+  onClose () {
+    // console.log('close:' + this.ws.readyState)
+    // console.log('已关闭websocket')
+    this.ws.close()
   }
-
   // 当连接发生错误
-  _onError() {
-    this.errorHandler({
-      message: "连接错误",
-      type: "error",
-    });
+  onError () {
+    // console.log('error' + this.ws.readyState)
+    // console.log('websocket连接失败')
     // 连接失败后，断线重练
     setTimeout(() => {
-      console.log("websocket重新连接");
-      this.init();
-    }, 1000);
+      this.init()
+    }, 1000)
   }
 
   // 心跳检测
-  checkServer() {
-    this.checkCount = 0;
-    clearInterval(this.handle);
-    // 当每一次测试心跳检测数据的时间超过了所设时间，则断开重连
-    this.handle = setInterval(() => {
-      // 如果超过重连次数限制则主动断开连接
-      if (this.checkCount >= this.limitCheckCount) {
-        clearInterval(this.handle);
-        this._onClose();
-        console.log("服务器已断开连接");
-      } else {
-        this.checkCount++;
-        this._onClose();
-        this._onError();
-      }
-    }, this.timeInterval + 1000);
+  checkServer () {
+    clearTimeout(this.handle)
+    // 当每一次测试心跳检测数据的时间超过规定时间，则断开重连
+    this.handle = setTimeout(() => {
+      this.onClose()
+      this.onError()
+    }, this.timeInterval)
   }
 }
 
-export default WebSocketClient;
+export default webSocketClient
